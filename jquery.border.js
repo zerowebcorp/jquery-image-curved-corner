72a761b839a3660e5aef819700b19eb32924a86e
Vivek V <vivek@qburst.com>
2011-11-22 09:37:48 +0530

    Added options

--- jquery.border.js
+++ jquery.border.js
@@ -1,8 +1,24 @@
+/*
+jQuery CSS3 Border Plugin
+Vivek
+getvivekv@gmail.com
+*/
+
 (function($){
      $.fn.border = function (options) {
+
+	var defaults = {
+		radius : '1em 4em 1em 4em'
+	};
+
+	var options = $.extend(defaults,options);
+
 		return this.each(function(){
 			var obj = $(this);
-				obj.css({"border-radius": '1em 4em 1em 4em'},{"-moz-border-radius": "1em 4em 1em 4em"});
+			obj.css(
+				{"border-radius": options.radius },
+				{"-moz-border-radius": options.radius}
+			);
 		});
 	}
 })(jQuery);
