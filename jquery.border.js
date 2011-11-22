a2ac274ff10651f1a0331c96a7e86a150be140b5
Vivek V <vivek@qburst.com>
2011-11-22 08:59:13 +0530

    Added sample images and index.html

--- jquery.border.js
+++ jquery.border.js
@@ -1,11 +1,8 @@
 (function($){
- 
-    $.fn.border = function (options) {
+     $.fn.border = function (options) {
 		return this.each(function(){
-			
 			var obj = $(this);
-				obj.css({"border-radius": '1em 4em 1em 4em'});
+				obj.css({"border-radius": '1em 4em 1em 4em'},{"-moz-border-radius": "1em 4em 1em 4em"});
 		});
 	}
-	
 })(jQuery);
